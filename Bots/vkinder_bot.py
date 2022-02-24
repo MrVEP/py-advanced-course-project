5606f45569689f963e3c72050c9e85cd5fd8d9d6
Vasily Pogreev <vasily.vep@gmail.com>
2022-02-25 00:36:14 +0300

    Added script for working when db is not accessible

--- Bots/vkinder_bot.py
+++ Bots/vkinder_bot.py
@@ -1,4 +1,6 @@
 import vk_api
+import sqlalchemy
+import json
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
 from VKapi.user_data import VKUser
@@ -29,12 +31,70 @@ def get_age_range(response: str) -> list[int] or bool:
             return False
 
 
+def get_backup():
+    with open('temp_backup.json', encoding='utf-8') as file:
+        data = json.load(file)
+        return data
+
+
+def save_backup(data):
+    with open('temp_backup.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=2)
+
+
+def push_backup_to_db():
+    data = get_backup()
+    empty_backup_template = '''{
+      "users": {
+
+      },
+      "results": {
+
+      }
+    }'''
+    for i in data['users'].keys():
+        i = int(i)
+        temp_user = VKUser(i)
+        temp_user.first_name = data['users'][f'{i}'][0]
+        temp_user.last_name = data['users'][f'{i}'][1]
+        temp_user.age = data['users'][f'{i}'][2]
+        temp_user.sex = data['users'][f'{i}'][3]
+        temp_user.city = data['users'][f'{i}'][4]
+        temp_user.city_id = data['users'][f'{i}'][5]
+        temp_user.age_range = data['users'][f'{i}'][6]
+        if i not in db.get_all_users_id():
+            db.add_user(temp_user)
+        else:
+            db.change_info(temp_user)
+    for i in data['results'].keys():
+        for j in data['results'][i]:
+            if j not in db.get_old_results(i):
+                db.add_result(i, j)
+    with open('temp_backup.json', 'w', encoding='utf-8') as file:
+        file.write(empty_backup_template)
+
+
 def relationship_bot():
+    db_failed = False
     for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
             user_id = event.object.message['from_id']
             if event.object.message['text'] == 'Начать':
-                if user_id not in db.get_all_users_id():
+                try:
+                    database = db.get_all_users_id()
+                    push_backup_to_db()
+                except sqlalchemy.exc.OperationalError:
+                    db_failed = True
+                    data = get_backup()
+                    vk.messages.send(user_id=user_id,
+                                     message=f'К сожалению, база данных на данный момент не доступна. '
+                                             f'Вы все еще можете воспользоваться ботом, однако результаты могут '
+                                             f'повторяться с выдаными раннее.',
+                                     random_id=get_random_id())
+                    database = []
+                    for i in data['users'].keys():
+                        database.append(int(i))
+                if user_id not in database:
                     # Если бот имеет дело с новым пользователем, то сначала он заполнит его личные данные
                     # со страницы вк и спросит то, что не сможет получить с нее.
                     vk_user = VKUser(user_id)
@@ -175,10 +235,30 @@ def relationship_bot():
                                     keyboards.change_info(vk_user)
                                 break
                     # Сформировав класс VKUser со всеми данными, бот запишет их в БД
-                    db.add_user(vk_user)
+                    if db_failed:
+                        data['users'][f'{user_id}'] = [vk_user.first_name,
+                                                  vk_user.last_name,
+                                                  vk_user.age,
+                                                  vk_user.sex,
+                                                  vk_user.city,
+                                                  vk_user.city_id,
+                                                  vk_user.age_range]
+                        data['results'][f'{user_id}'] = []
+                    else:
+                        db.add_user(vk_user)
                 else:
                     # Если бот имеет дело с уже существующим пользователем, то он просто подтянет все данные из БД
-                    vk_user = db.get_user_info(user_id)
+                    if db_failed:
+                        vk_user = VKUser(user_id)
+                        vk_user.first_name = data['users'][f'{user_id}'][0]
+                        vk_user.last_name = data['users'][f'{user_id}'][1]
+                        vk_user.age = data['users'][f'{user_id}'][2]
+                        vk_user.sex = data['users'][f'{user_id}'][3]
+                        vk_user.city = data['users'][f'{user_id}'][4]
+                        vk_user.city_id = data['users'][f'{user_id}'][5]
+                        vk_user.age_range = data['users'][f'{user_id}'][6]
+                    else:
+                        vk_user = db.get_user_info(user_id)
                     vk.messages.send(user_id=user_id, message=f'С возвращением, {vk_user.first_name}!',
                                      random_id=get_random_id())
                 keyboards.search_or_change(vk_user)
@@ -262,15 +342,28 @@ def relationship_bot():
                                                 else:
                                                     correct_range = True
                                         break
-                            db.change_info(vk_user)
+                            if db_failed:
+                                data['users'][user_id] = [vk_user.first_name,
+                                                          vk_user.last_name,
+                                                          vk_user.age,
+                                                          vk_user.sex,
+                                                          vk_user.city,
+                                                          vk_user.city_id,
+                                                          vk_user.age_range]
+                            else:
+                                db.change_info(vk_user)
                             break
                         elif event.object.message['text'] == 'Начать поиск':
                             results = vk_api_scripts.search_pair(vk_user)
                             # Получив список id подходящих пар, бот найдет первое id, которого еще нет в БД,
                             # добавит его туда и выдаст полученный результат, после чего завершит свою работу.
                             new_pair_id = False
+                            if db_failed:
+                                old_results = data['results'][f'{user_id}']
+                            else:
+                                old_results = db.get_old_results(vk_user.id)
                             for result in results:
-                                if result not in db.get_old_results(vk_user.id):
+                                if result not in old_results:
                                     new_pair_id = result
                                     break
                             if not new_pair_id:
@@ -279,7 +372,10 @@ def relationship_bot():
                                                          f'\nИзмените возрастной диапазон.',
                                                  random_id=get_random_id())
                             else:
-                                db.add_result(vk_user.id, new_pair_id)
+                                if db_failed:
+                                    data['results'][f'{user_id}'].append(new_pair_id)
+                                else:
+                                    db.add_result(vk_user.id, new_pair_id)
                                 pair_top_pics = vk_api_scripts.get_result_info(new_pair_id)
                                 vk.messages.send(user_id=user_id,
                                                  message=f'Найден подходящий результат: '
@@ -289,6 +385,8 @@ def relationship_bot():
                                                              f'{pair_top_pics[2]}'],
                                                  random_id=get_random_id())
                             break
+                if db_failed:
+                    save_backup(data)
                 keyboards.getting_started(user_id)
             else:
                 keyboards.getting_started(user_id)
